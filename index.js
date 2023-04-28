ff9e96fb61c90968dfcb526f42426b2eacca7166
maheshst05 <maheshthombare005.com>
2023-04-28 12:54:02 +0530

    as

---
+++ index.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+let str = "okk"
+
+// eslint-disable-next-line no-unused-vars
+const chaasd = 'as'
+
+console.log(str)
+
+
+// eslint-disable-next-line no-constant-condition
+if(true){
+    console.log("true")
+}else{
+    console.log("false")
+}
